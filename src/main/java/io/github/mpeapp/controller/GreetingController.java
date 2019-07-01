4537ac3be204b9f8cc05782722736297e9123ba4
Guilherme de Lemos <guilherme.eti@gmail.com>
2019-06-30 21:21:22 -0300

    Error handling

--- src/main/java/io/github/mpeapp/controller/GreetingController.java
+++ src/main/java/io/github/mpeapp/controller/GreetingController.java
@@ -1,5 +1,7 @@
 package io.github.mpeapp.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
+
+    Logger log = LoggerFactory.getLogger(GreetingController.class);
 
     @GetMapping("/greeting")
     public String greeting(
@@ -17,9 +21,26 @@ public class GreetingController {
             ) String name,
             Model model
     ) {
-//        System.out.println("TESTE");
+        System.out.println("Não faça isso");
+        log.info("Faça isso");
+
         model.addAttribute("name", name);
         return "greeting";
+    }
+
+    @GetMapping("/error-test")
+    public String errorTest() {
+        throw new IllegalArgumentException("Error test");
+    }
+
+    @GetMapping("/log")
+    public String log() {
+        log.trace("A TRACE Message");
+        log.debug("A DEBUG Message");
+        log.info("An INFO Message");
+        log.warn("A WARN Message");
+        log.error("An ERROR Message");
+        return "index";
     }
 
 }
